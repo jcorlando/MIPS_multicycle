9c7a823ff14499cf24084e7b85660af0d6fe270c
Juan Orlando <jcorlandomc@yahoo.com>
2020-10-07 16:09:25 -0700

    10/07/2020 04:09 pm

--- MIPS_multicycle.srcs/sources_1/new/top.v
+++ MIPS_multicycle.srcs/sources_1/new/top.v
@@ -23,6 +23,7 @@ module top # ( parameter WL = 32 )
     wire [WL - 1 : 0] RD1;
     wire [WL - 1 : 0] RD2;
     wire [WL - 1 : 0] A;
+    wire [WL - 1 : 0] B;
     wire signed [WL - 1 : 0] SImm = inst_reg.SImm;
     wire signed [WL - 1 : 0] ALUResult;
     wire [WL - 1 : 0] Adr;
@@ -35,7 +36,7 @@ module top # ( parameter WL = 32 )
     // Control Unit
     control_unit cont_unit( .CLK(CLK), .RESET(RESET), .Op(inst_reg.opcode), .Funct(inst_reg.funct), .ALUSrcA(ALUSrcA),  // Control Unit
                    .ALUSrcB(ALUSrcB), .IorD(IorD), .ALUControl(ALUControl), .PCSrc(PCSrc), .IRWrite(IRWrite),           // Control Unit
-                        .PCWrite(PCWrite), .RegWrite(RegWrite) );                                                       // Control Unit
+                        .PCWrite(PCWrite), .RegWrite(RegWrite), .MemWrite(MemWrite) );                                  // Control Unit
     // Control Unit
     
     // program counter
@@ -47,40 +48,40 @@ module top # ( parameter WL = 32 )
     // program counter multiplexer
     
     // Data Memory
-    data_Mem data_mem( .CLK(CLK), .WE(MemWrite), .A(Adr), .RD(RD) );                                             // Data Memory
+    data_Mem data_mem( .CLK(CLK), .WE(MemWrite), .A(Adr), .RD(RD), .WD(B) );                             // Data Memory
     // Data Memory
     
     // Instruction Register
-    inst_reg inst_reg( .CLK(CLK), .EN(IRWrite), .instr_in(RD), .instr_out(instr_out) );                       // Instruction Register
+    inst_reg inst_reg( .CLK(CLK), .EN(IRWrite), .instr_in(RD), .instr_out(instr_out) );                  // Instruction Register
     // Instruction Register
     
     // data register
-    data_register data_register( .CLK(CLK), .RD(RD), .Data(Data) );                                             // data register
+    data_register data_register( .CLK(CLK), .RD(RD), .Data(Data) );                                       // data register
     // data register
     
     // Register File
-    reg_File reg_file( .CLK(CLK), .WE3(RegWrite), .A1(inst_reg.rs), .A2(inst_reg.rt), .WD3(Data),               // Register File
-                    .A3(inst_reg.rt), .RD1(RD1), .RD2(RD2) );                                                   // Register File
+    reg_File reg_file( .CLK(CLK), .WE3(RegWrite), .A1(inst_reg.rs), .A2(inst_reg.rt), .WD3(Data),          // Register File
+                    .A3(inst_reg.rt), .RD1(RD1), .RD2(RD2) );                                              // Register File
     // Register File
     
     // Register File register
-    reg_file_register  reg_file_reg( .CLK(CLK), .RD1_in(RD1), .RD2_in(RD2), .A(A) );                            // Register File register
+    reg_file_register  reg_file_reg( .CLK(CLK), .RD1_in(RD1), .RD2_in(RD2), .A(A), .B(B) );                // Register File register
     // Register File register
     
     // ALUSrcA multiplexer
-    ALUSrcA_multiplexer ALUSrcA_multiplexer( .ALUSrcA(ALUSrcA), .PC(pc_Out), .A(A), .SrcA(SrcA) );              // ALUSrcA multiplexer
+    ALUSrcA_multiplexer ALUSrcA_multiplexer( .ALUSrcA(ALUSrcA), .PC(pc_Out), .A(A), .SrcA(SrcA) );         // ALUSrcA multiplexer
     // ALUSrcA multiplexer
     
     // ALUSrcB multiplexer
-    ALUSrcB_multiplexer ALUSrcB_multiplexer( .ALUSrcB(ALUSrcB), .A(), .SignImm(SImm), .D(), .SrcB(SrcB) );      // ALUSrcB multiplexer
+    ALUSrcB_multiplexer ALUSrcB_multiplexer( .ALUSrcB(ALUSrcB), .A(), .SignImm(SImm), .D(), .SrcB(SrcB) );  // ALUSrcB multiplexer
     // ALUSrcB multiplexer
     
     // ALU
-    alu alu( .A(SrcA), .B(SrcB), .ALU_Control(ALUControl), .ALU_Out(ALUResult) );                               // ALU
+    alu alu( .A(SrcA), .B(SrcB), .ALU_Control(ALUControl), .ALU_Out(ALUResult) );                           // ALU
     // ALU
     
     // ALU register
-    alu_out_register alu_out_reg( .CLK(CLK), .ALU_reg_in(ALUResult), .ALU_reg_out(ALU_reg_out) );               // ALU register
+    alu_out_register alu_out_reg( .CLK(CLK), .ALU_reg_in(ALUResult), .ALU_reg_out(ALU_reg_out) );           // ALU register
     // ALU register
     
 endmodule
